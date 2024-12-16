06b85fea7f055994cb8e63dcfe9d6f7588304ddc
jw7331 <121295472+jw7331@users.noreply.github.com>
2024-12-16 15:39:26 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.icon').addEventListener('click', () => {
+    window.scrollTo({
+        top: window.innerHeight, // Scroll to second slide
+        behavior: 'smooth'
+    });
+});
