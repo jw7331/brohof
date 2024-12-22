e24f3a225fd9a632c78effcceb010f386b6ddce7
jw7331 <121295472+jw7331@users.noreply.github.com>
2024-12-22 21:42:49 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.icon').addEventListener('click', () => {
-    window.scrollTo({
-        top: window.innerHeight, // Scroll to second slide
-        behavior: 'smooth'
-    });
-});
